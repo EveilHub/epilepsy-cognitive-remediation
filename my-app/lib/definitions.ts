24b6c7be1f8bc6457af309626c69255466ee1b53
EveilHub <EveilHub@proton.me>
2026-04-07 15:34:27 +0200

    created for content task & types

---
+++ my-app/lib/definitions.ts
@@ -0,0 +1,14 @@
+export type MemoTravailTypes = {
+    sequChiff: {
+        title: string;
+        description: string;
+    },
+    nback: {
+        title: string;
+        description: string;
+    },
+    grillePoint: {
+        title: string;
+        description: string;
+    }
+}
---
+++ my-app/lib/tasks-content.ts
@@ -0,0 +1,16 @@
+import { MemoTravailTypes } from "./definitions";
+
+export const MemoTravailContent: MemoTravailTypes = {
+    sequChiff: {
+        title: "Séquences de chiffres:",
+        description: "Mémoriser une suite, la répéter à l'envers"
+    },
+    nback: {
+        title: "N-back:",
+        description: "Indiquer si le stimulus actuel correspond à celui d'il y a N étapes",
+    },
+    grillePoint: {
+        title: "Grilles de points:",
+        description: "Mémoriser des positions, les reproduire"
+    }
+};
