97692b3804023fec51e91e60720243365f5d893a
EveilHub <EveilHub@proton.me>
2026-04-13 16:23:14 +0200

    darkmode resolved

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" style={{colorScheme:"dark"}}>
       <body className="min-h-full flex flex-col">
         <ThemeProvider>
           <Switcher />
