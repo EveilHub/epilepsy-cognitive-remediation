f920f15a55e3adc04363774bdd95214dbd347fde
EveilHub <EveilHub@proton.me>
2026-04-15 11:37:28 +0200

    news routes created for submits

---
+++ my-app/app/api/epilepsy-submit/route.ts
@@ -0,0 +1,4 @@
+async function POSTEPI(req: Request, res: Response): Promise<void> {
+    const data = req.body;
+    console.log(data);
+};
---
+++ my-app/app/api/patient-submit/route.ts
@@ -0,0 +1,4 @@
+async function POST(req: Request, res: Response): Promise<void> {
+    const data = req.body;
+    console.log(data);
+};
